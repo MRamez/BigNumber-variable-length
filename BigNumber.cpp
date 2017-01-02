7ff3844181b4193af9611c2a68b37b3e070014ac
MrDetective <mehdiramezanister@gmail.com>
2017-01-02 21:41:20 +0330

    Added BigNumber.h and BigNumber.cpp

---
+++ BigNumber.cpp
@@ -0,0 +1,310 @@
+#include "BigNumber.h"
+
+namespace Big_Number
+{
+
+	//definition of static members
+	const BigNumber BigNumber::big0;
+	const BigNumber BigNumber::big1("1");
+	const BigNumber BigNumber::big2("2");
+
+	//definition of member functinos
+	BigNumber::BigNumber(const std::string& s): sign(true)
+	{
+		if (s.empty() || (s.size() == 1 && (s[0] == '-' || s[0] == '+')))
+		{
+			digits.push_back(0);
+			return;
+		}
+		if (s[0] == '-')
+			sign = false;
+		std::size_t pos = 0;
+		if (!std::isdigit(s[0]))
+			pos = 1;
+		digits.reserve(s.size() - pos);
+		for (std::size_t i = s.size(); i-- != pos; )
+			digits.push_back(s[i] - '0');
+		discard_leading_zeros();
+	}
+
+	BigNumber& BigNumber::operator+=(const BigNumber& rhs)
+	{
+		if (this->sign != rhs.sign)
+		{
+			if (this->sign)
+				*this -= -rhs;
+			else
+				*this = rhs - -*this;
+		}
+		else
+		{
+			digits.reserve(std::max(size(), rhs.size()) + 1);
+			unsigned char carry = 0;
+			for (std::size_t i = 0; i < size() && i < rhs.size(); ++i)
+			{
+				digits[i] = digits[i] + rhs.digits[i] + carry;
+				carry = digits[i] / 10;
+				digits[i] %= 10;
+			}
+			if (size() > rhs.size())
+			{
+				for (std::size_t i = rhs.size(); i < size(); ++i)
+				{
+					digits[i] = digits[i] + carry;
+					carry = digits[i] / 10;
+					digits[i] %= 10;
+				}
+			}
+			else if (rhs.size() > size())
+			{
+				for (std::size_t i = size(); i < rhs.size(); ++i)
+				{
+					digits.push_back(rhs.digits[i] + carry);
+					carry = digits[i] / 10;
+					digits[i] %= 10;
+				}
+			}
+			if (carry)
+				digits.push_back(carry);
+		}
+		return *this;
+	}
+
+	BigNumber& BigNumber::operator<<=(std::size_t n)
+	{
+		digits.reserve(size() + n);
+		std::size_t initial_size = size();
+		if (n > initial_size)
+		{
+			digits.insert(digits.end(), n - initial_size, 0);
+			for (std::size_t i = 0; i < initial_size; ++i)
+				digits.push_back(digits[i]);
+			std::fill(digits.begin(), digits.begin() + initial_size, 0);
+		}
+		else
+		{
+			for (std::size_t i = 0; i < n; ++i)
+				digits.push_back(digits[initial_size + i - n]);
+			for (std::size_t i = initial_size; i-- != n; )
+				digits[i] = digits[i - n];
+			std::fill(digits.begin(), digits.begin() + n, 0);
+		}
+		discard_leading_zeros(); //useful when shifting zero
+		return *this;
+	}
+
+	BigNumber& BigNumber::operator*=(const BigNumber& rhs)
+	{
+		BigNumber product;
+		product.digits.reserve(size() + rhs.size());
+		bool initial_this_sign = this->sign;
+		bool initial_rhs_sign = rhs.sign; //because rhs can be *this!
+		for (std::size_t i = rhs.size(); i-- != 0; )
+		{
+			product <<= 1;
+			product += this->mult_digit(rhs.digits[i]);
+		}
+		*this = std::move(product); //sign of product is true
+		if (initial_rhs_sign != initial_this_sign && *this != big0)
+			this->sign = false;
+		return *this;
+	}
+
+	BigNumber& BigNumber::operator>>=(std::size_t n)
+	{
+		if (size() <= n)
+			return *this = big0;
+		for (std::size_t i = 0; i < size() - n; ++i)
+			digits[i] = digits[i + n];
+		digits.erase(digits.end() - n, digits.end());
+		return *this;
+	}
+
+	BigNumber& BigNumber::operator/=(const BigNumber& rhs)
+	{
+		if (rhs == big0)
+			throw DivisionByZero("Error: Division by zero.");
+		bool initial_this_sign = this->sign;
+		bool initial_rhs_sign = rhs.sign; //because rhs can be *this!
+		std::size_t initial_shift;
+		if (size() <= rhs.size())
+			initial_shift = 0;
+		else
+			initial_shift = size() - rhs.size();
+		BigNumber denom = rhs << initial_shift;
+		denom.sign = true;
+		BigNumber num = std::move(*this);
+		num.sign = true;
+		BigNumber q;
+		for (int i = 0; i <= initial_shift; ++i)
+		{
+			unsigned char q_digit = 0;
+			while (num >= denom)
+			{
+				num -= denom;
+				++q_digit;
+			}
+			q <<= 1;
+			q.digits[0] = q_digit;
+			denom >>= 1;
+		}
+		*this = std::move(q); //sign of q is true
+		if (initial_this_sign != initial_rhs_sign && *this != big0)
+			this->sign = false;
+		return *this;
+	}
+
+	BigNumber& BigNumber::operator-=(const BigNumber& rhs)
+	{
+		if (this->sign != rhs.sign)
+		{
+			bool final_sign = this->sign;
+			this->sign = true;
+			*this += abs(rhs);
+			this->sign = final_sign;
+		}
+		else
+		{
+			bool final_sign;
+			const BigNumber* mi;
+			const BigNumber* ma;
+			if (*this >= rhs)
+			{
+				final_sign = true;
+				if (!this->sign)
+				{
+					mi = this;
+					ma = &rhs;
+				}
+				else
+				{
+					mi = &rhs;
+					ma = this;
+				}
+			}
+			else
+			{
+				final_sign = false;
+				if (this->sign)
+				{
+					mi = this;
+					ma = &rhs;
+				}
+				else
+				{
+					mi = &rhs;
+					ma = this;
+				}
+			}
+			bool borrow = false;
+			for (std::size_t i = 0; i < ma->size(); ++i)
+			{
+				unsigned char sub = borrow;
+				if (i < mi->size())
+					sub += mi->digits[i];
+				unsigned char resulted_digit;
+				if (sub > ma->digits[i])
+				{
+					borrow = true;
+					resulted_digit = 10 + ma->digits[i] - sub;
+				}
+				else
+				{
+					borrow = false;
+					resulted_digit = ma->digits[i] - sub;
+				}
+				if (i == this->size()) //no need to write i >= this->size() because this->size() is growing with i
+					this->digits.push_back(resulted_digit);
+				else
+					this->digits[i] = resulted_digit;
+			}
+			discard_leading_zeros();
+			this->sign = final_sign;
+		}
+		return *this;
+	}
+
+	BigNumber& BigNumber::operator^=(const BigNumber& rhs)
+	{
+		//digits.reserve is not practical here, because a^b has floor(b * log10(a)) + 1 digits and b is a BigNumber.
+		if (!rhs.sign)
+			throw NegativePower("Error: Power is negative.");
+		if (rhs == big0)
+			return *this = big1;
+		if (rhs == big1)
+			return *this;
+		if (is_even(rhs))
+		{
+			*this ^= rhs / big2;
+			*this *= *this;
+		}
+		else
+		{
+			BigNumber temp = *this; //because rhs can be *this!
+			*this ^= (rhs - big1) / big2;
+			*this *= *this;
+			*this *= temp;
+		}
+		if (is_even(rhs))
+			this->sign = true;
+		return *this;
+	}
+
+	BigNumber BigNumber::mult_digit(unsigned char n) const //n should belong to [0, 9]
+	{
+		if (n == 0)
+			return big0;
+		BigNumber ret = *this;
+		ret.sign = true;
+		unsigned char carry = 0;
+		for (std::size_t i = 0; i < size(); ++i)
+		{
+			ret.digits[i] = n * digits[i] + carry;
+			carry = ret.digits[i] / 10;
+			ret.digits[i] %= 10;
+		}
+		if (carry)
+			ret.digits.push_back(carry);
+		return ret;
+	}
+
+	//definition of friend functions
+	bool operator<(const BigNumber& lhs, const BigNumber& rhs)
+	{
+		if (!lhs.sign && rhs.sign)
+			return true;
+		else if (lhs.sign && !rhs.sign)
+			return false;
+		else if (!lhs.sign && !rhs.sign)
+			return -rhs < -lhs;
+		else
+		{
+			if (lhs.size() < rhs.size())
+				return true;
+			else if (lhs.size() > rhs.size())
+				return false;
+			else
+			{
+				for (std::size_t i = lhs.size(); i-- != 0; )
+				{
+					if (lhs.digits[i] < rhs.digits[i])
+						return true;
+					else if (rhs.digits[i] < lhs.digits[i])
+						return false;
+				}
+				return false;
+			}
+		}
+	}
+
+} //end of namespace Big_Number
+
+namespace std
+{
+	size_t hash<Big_Number::BigNumber>::operator()(const Big_Number::BigNumber& number) const
+	{
+		ostringstream oss;
+		oss << number;
+		return hash<string>()(oss.str());
+	}
+}
---
+++ BigNumber.h
@@ -0,0 +1,290 @@
+#ifndef BIGNUMBER_H
+#define BIGNUMBER_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <stdexcept>
+#include <type_traits>
+#include <sstream>
+#include <cctype>
+
+namespace Big_Number
+{
+
+	class DivisionByZero: public std::invalid_argument
+	{
+		public:
+			explicit DivisionByZero(const std::string& msg): std::invalid_argument(msg) {}
+	};
+
+	class NegativePower: public std::invalid_argument
+	{
+		public:
+			explicit NegativePower(const std::string& msg): std::invalid_argument(msg) {}
+	};
+
+	class BigNumber
+	{
+		std::vector<unsigned char> digits;
+		bool sign;
+
+		public:
+			BigNumber(): sign(true) { digits.push_back(0); }
+			BigNumber(const std::string&);
+			BigNumber(const char* ca): BigNumber(std::string(ca)) {}
+			BigNumber(int n): BigNumber(std::to_string(n)) {} //This is needed, because without it, 0 cannot be automatically converted to a BigNumber.
+			BigNumber(long long int n): BigNumber(std::to_string(n)) {}
+			
+			unsigned char& operator[](std::size_t i) { return digits[i]; }
+			const unsigned char& operator[](std::size_t i) const { return digits[i]; }
+			unsigned char& at(std::size_t i) { return digits.at(i); }
+			const unsigned char& at(std::size_t i) const { return digits.at(i); }
+			std::size_t size() const { return digits.size(); }
+			BigNumber& operator+=(const BigNumber&);
+			BigNumber& operator<<=(std::size_t);
+			BigNumber& operator*=(const BigNumber&);
+			BigNumber& operator>>=(std::size_t);
+			BigNumber& operator/=(const BigNumber&);
+			BigNumber operator-() const;
+			BigNumber& operator-=(const BigNumber&);
+			BigNumber& operator%=(const BigNumber&);
+			BigNumber& operator^=(const BigNumber&);
+			BigNumber& operator++() { return *this += big1; }
+			BigNumber& operator--() { return *this -= big1; }
+			BigNumber operator++(int);
+			BigNumber operator--(int);
+
+		private:
+			static const BigNumber big0;
+			static const BigNumber big1;
+			static const BigNumber big2;
+
+			BigNumber mult_digit(unsigned char) const; //accepts only a digit.
+			void discard_leading_zeros();
+
+		friend BigNumber operator<<(const BigNumber&, std::size_t);
+		friend BigNumber operator+(const BigNumber&, const BigNumber&);
+		friend BigNumber operator*(const BigNumber&, const BigNumber&);
+		friend BigNumber operator>>(const BigNumber&, std::size_t);
+		friend BigNumber operator/(const BigNumber&, const BigNumber&);
+		friend BigNumber operator-(const BigNumber&, const BigNumber&);
+		friend BigNumber operator%(const BigNumber&, const BigNumber&);
+		friend BigNumber abs(const BigNumber&);
+		friend BigNumber max(const BigNumber&, const BigNumber&);
+		friend BigNumber min(const BigNumber&, const BigNumber&);
+		friend bool is_even(const BigNumber&);
+		friend bool is_odd(const BigNumber&);
+		friend BigNumber operator^(const BigNumber&, const BigNumber&);
+		friend bool operator<(const BigNumber&, const BigNumber&);
+		friend bool operator>(const BigNumber&, const BigNumber&);
+		friend bool operator<=(const BigNumber&, const BigNumber&);
+		friend bool operator>=(const BigNumber&, const BigNumber&);
+		friend bool operator==(const BigNumber&, const BigNumber&);
+		friend bool operator!=(const BigNumber&, const BigNumber&);
+		friend std::ostream& operator<<(std::ostream&, const BigNumber&);
+		friend std::istream& operator>>(std::istream&, BigNumber&);
+		friend void swap(BigNumber&, BigNumber&);
+	};
+
+	//definition of inline member functions
+	inline BigNumber BigNumber::operator-() const
+	{
+		if (*this == big0)
+			return big0;
+		BigNumber ret = *this;
+		if (ret.sign)
+			ret.sign = false;
+		else
+			ret.sign = true;
+		return ret;
+	}
+
+	inline BigNumber& BigNumber::operator%=(const BigNumber& rhs)
+	{
+		*this -= (*this / rhs) * rhs;
+		return *this;
+	}
+
+	inline BigNumber BigNumber::operator++(int)
+	{
+		BigNumber ret = *this;
+		++*this;
+		return ret;
+	}
+
+	inline BigNumber BigNumber::operator--(int)
+	{
+		BigNumber ret = *this;
+		--*this;
+		return ret;
+	}
+
+	inline void BigNumber::discard_leading_zeros() //Assumption: end != 0
+	{
+		auto riter = std::find_if(digits.rbegin(), digits.rend(), [] (unsigned char c) { return c != 0; });
+		digits.erase(riter.base(), digits.end());
+		if (size() == 0)
+		{
+			digits.push_back(0);
+			sign = true;
+		}
+	}
+
+	//definition of inlined friend functions
+	inline BigNumber operator<<(const BigNumber& lhs, std::size_t n)
+	{
+		BigNumber ret = lhs;
+		ret <<= n;
+		return ret;
+	}
+
+	inline BigNumber operator+(const BigNumber& lhs, const BigNumber& rhs)
+	{
+		BigNumber temp = lhs;
+		temp += rhs;
+		return temp;
+	}
+
+	inline BigNumber operator*(const BigNumber& lhs, const BigNumber& rhs)
+	{
+		BigNumber ret = lhs;
+		ret *= rhs;
+		return ret;
+	}
+
+	inline BigNumber operator>>(const BigNumber& lhs, std::size_t n)
+	{
+		BigNumber ret = lhs;
+		ret >>= n;
+		return ret;
+	}
+
+	inline BigNumber operator/(const BigNumber& lhs, const BigNumber& rhs)
+	{
+		BigNumber ret = lhs;
+		ret /= rhs;
+		return ret;
+	}
+
+	inline BigNumber operator-(const BigNumber& lhs, const BigNumber& rhs)
+	{
+		BigNumber ret = lhs;
+		ret -= rhs;
+		return ret;
+	}
+
+	inline BigNumber operator%(const BigNumber& lhs, const BigNumber& rhs)
+	{
+		BigNumber ret = lhs;
+		ret %= rhs;
+		return ret;
+	}
+
+	inline BigNumber abs(const BigNumber& bn)
+	{
+		if (bn.sign)
+			return bn;
+		else
+			return -bn;
+	}
+
+	inline BigNumber max(const BigNumber& bn1, const BigNumber& bn2)
+	{
+		if (bn1 < bn2)
+			return bn2;
+		else
+			return bn1;
+	}
+
+	inline BigNumber min(const BigNumber& bn1, const BigNumber& bn2)
+	{
+		if (bn1 < bn2)
+			return bn1;
+		else
+			return bn2;
+	}
+
+	inline bool is_even(const BigNumber& bn)
+	{
+		return bn.digits[0] % 2 == 0;
+	}
+
+	inline bool is_odd(const BigNumber& bn)
+	{
+		return !is_even(bn);
+	}
+
+	inline BigNumber operator^(const BigNumber& lhs, const BigNumber& rhs)
+	{
+		BigNumber ret = lhs;
+		ret ^= rhs;
+		return ret;
+	}
+
+	inline bool operator>(const BigNumber& lhs, const BigNumber& rhs)
+	{
+		return rhs < lhs;
+	}
+
+	inline bool operator<=(const BigNumber& lhs, const BigNumber& rhs)
+	{
+		return !(rhs < lhs);
+	}
+
+	inline bool operator>=(const BigNumber& lhs, const BigNumber& rhs)
+	{
+		return !(lhs < rhs);
+	}
+
+	inline bool operator==(const BigNumber& lhs, const BigNumber& rhs)
+	{
+		return !(lhs < rhs) && !(rhs < lhs);
+	}
+
+	inline bool operator!=(const BigNumber& lhs, const BigNumber& rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	inline std::ostream& operator<<(std::ostream& os, const BigNumber& bn)
+	{
+		if (!bn.sign)
+			os<<'-';
+		for (std::size_t i = bn.size(); i-- != 0;)
+			os<<(unsigned)(bn.digits[i]);
+		return os;
+	}
+
+	inline std::istream& operator>>(std::istream& is, BigNumber& bn)
+	{
+		std::string s;
+		is>>s;
+		bn = BigNumber(s);
+		return is;
+	}
+
+	inline void swap(BigNumber& lhs, BigNumber& rhs)
+	{
+		using std::swap;
+		swap(lhs.digits, rhs.digits);
+		swap(lhs.sign, rhs.sign);
+	}
+
+} //end of namespace Big_Number
+
+namespace std
+{
+	template <>
+	struct hash<Big_Number::BigNumber>
+	{
+		typedef size_t result_type;
+		typedef Big_Number::BigNumber argument_type;
+
+		size_t operator()(const Big_Number::BigNumber&) const;
+	};
+}
+
+#endif
